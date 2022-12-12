1886505a63c368e04735cd1cac6d2487a97c5c4c
bharatrai53 <bharatrai2016@gmail.com>
2022-12-11 20:50:37 -0800

    fixed local storage

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # workDayScheduler
+
+## Description
+The Work Day Scheduler was created so that I could utilize my skills in Bootstrap and JQuery to better understand the syntax of these new tools within my code. I was also able to further refine my understadning of localStorage by saving and retrieving items from the localStorage. Lastly, I was able to integrate the DayJs Library to get the current time and use it as a comparator in some functions as well. The time scheduler is a useful tool that can be used to help manage tasks and create a list of items to accomplish by the end of the day. 
+
+## Usage
+When you type text in a row and select the save icon you will be able to save your to do item and it will not disappear if you refresh the page. The color of each row block is defined based on the current time of the day. The hours of the past will appear as gray, the color of the present will be red, and the colors of the future rows will be green so that you can keep track of the day as it progresses. 
+
+## Screenshot
+
+## Deployed Link
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready( function () {
 
 var past = $('.past');
 var present = $('.present');
 var future = $('.future');
-var saveButton = $('.saveBtn');
-var hour = $('');
+var saveButton = $('#saveBtn');
 var hourNine = $('#9');
 var hourTen = $('#10');
 var hourEleven = $('#11');
@@ -21,7 +21,7 @@ var description = $(".description");
 
 var currentHour = dayjs().format('H');
 //var currentHour = 14;
-//console.log(currentHour);
+console.log(currentHour);
 var allTimes = [hourNine, hourTen, hourEleven, hourTwelve, hourOne, hourTwo, hourThree, hourFour, hourFive];
 //console.log(allTimes);
 
@@ -34,7 +34,7 @@ $('#currentDay').text(currentDate);
   description.each(function () {
   
   for (i=0; i < 9; i++) {
-    console.log(allTimes[i].attr("id"));
+   
     if (currentHour == allTimes[i].attr("id")) { 
       allTimes[i].removeClass("future");
       allTimes[i].removeClass("past");
@@ -53,7 +53,7 @@ $('#currentDay').text(currentDate);
     }
   });
 
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -61,19 +61,28 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  saveButton.on('submit', handleFormSubmit);
-
-  var handleFormSubmit = function (event) {
-    event.preventDefault();
-    console.log("save bttn works");
-  }
-
-  
-
+ 
+  $('.btn').on('click', function() {
+    let task = $(this).siblings(".description").val();
+    console.log(task);
+    let time = $(this).parent().attr('id');
+    localStorage.setItem(time, task);
+  });
+ 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-    
+  $('#9 .description').val(localStorage.getItem('9'));
+  $('#10 .description').val(localStorage.getItem('10'));
+  $('#11 .description').val(localStorage.getItem('11'));
+  $('#12 .description').val(localStorage.getItem('12'));
+  $('#13 .description').val(localStorage.getItem('13'));
+  $('#14 .description').val(localStorage.getItem('14'));
+  $('#15 .description').val(localStorage.getItem('15'));
+  $('#16 .description').val(localStorage.getItem('16'));
+  $('#17 .description').val(localStorage.getItem('17'));
+  
+  
 });
 
